09471caf493a070075f1136a89cc660c45a2e2fd
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-02-12 21:29:43 -0500

    chore: adds getPokemon function
    [optional body]
    
    [optional footer(s)]

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-if (process.env.NODE_ENV === 'development') {
+/* if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser');
   worker.start();
-}
+} */
+
+console.log('test');
+
+//get axios
+axios.get('https://pokeapi.co/api/v2/pokemon/pikachu').then((res) => {
+  console.log(res);
+});
