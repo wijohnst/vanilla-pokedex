3fcc20b9fccc3b49c423fb8fd46f206d5e1c5b8d
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-02-18 15:58:04 -0500

    chore: getPokemon add
    [optional body]
    
    [optional footer(s)]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-/* if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser');
   worker.start();
-} */
+}
 
-console.log('test');
+function getPokemon(name) {
+  axios
+    .get('https://pokeapi.co/api/v2/pokemon/' + name)
+    .then((res) => console.log(res.data))
+    .catch((err) => {
+      if (err.response) {
+        //server responded with other than 200/success
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
 
-//get axios
-axios.get('https://pokeapi.co/api/v2/pokemon/pikachu').then((res) => {
-  console.log(res);
-});
+        if (err.response.status === 404) {
+          console.log('No Pokemon found, try again');
+        }
+      } else if (err.request) {
+        //request made, no response
+        console.error(err.request);
+      } else {
+        //any other err, display err message
+        console.error(err.message);
+      }
+    });
+}
+
+getPokemon('8709097');
