37e04b343a81ba7a79a3d395ab66ad34b3d2c2da
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-02-25 21:11:36 -0500

    feat: Add Searchbar functionality
    [optional body]
    
    [optional footer(s)]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-if (process.env.NODE_ENV === 'development') {
+/* if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser');
   worker.start();
-}
+} */
 
+//DOM selectors
+const selectElement = document.getElementById('search-option');
+const inputElement = document.querySelector('input[id="search-input"]');
+const errorSpan = document.getElementById('error-span');
+const goButton = document.querySelector('button[class="go-button"]');
+
+//Dynamic Placeholder Text
+selectElement?.addEventListener('change', (e) => {
+  const selectedOption = selectElement.options[selectElement.selectedIndex];
+  if (selectedOption.value === 'id') {
+    inputElement.placeholder = 'Find Pokémon by Id';
+  } else if (selectedOption.value === 'name') {
+    inputElement.placeholder = 'Find Pokémon by Name';
+  }
+});
+
+//Use axios to get pokemon response object
 function getPokemon(name) {
   axios
     .get('https://pokeapi.co/api/v2/pokemon/' + name)
@@ -27,4 +44,16 @@ function getPokemon(name) {
     });
 }
 
-getPokemon('8709097');
+//pressing GO executes function or error validation
+goButton?.addEventListener('click', (e) => {
+  console.log('click');
+  e.preventDefault();
+  errorSpan.innerHTML = '';
+  if (inputElement.value === '') {
+    errorSpan.innerHTML = 'Please enter a Pokemon Name or ID';
+  } else {
+    getPokemon(inputElement.value);
+  }
+});
+
+//getPokemon('pikachu');
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,10 +2,6 @@ import { rest } from 'msw';
 
 export const handlers = [
   rest.get('/https://pokeapi.co/api/v2/pokemon/', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        welcomeMessage: 'Hello World!',
-      })
-    );
+    return res(ctx.json(res.data));
   }),
 ];
