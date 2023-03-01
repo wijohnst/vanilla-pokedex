ad2fba9ebbadf04acfe5e5195c4e0d15ee813553
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-03-01 10:12:31 -0500

    feat: Pokemon Details section
    [optional body]
    
    [optional footer(s)]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-if (process.env.NODE_ENV === 'development') {
+/* if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser');
   worker.start();
-}
+} */
+
+//DOM selectors
+const form = document.getElementById('search-form');
+const input = document.getElementById('search-input');
+const blankDetails = document.getElementById('empty-pokemon-details');
+const filledDetails = document.getElementById('filled-pokemon-details');
+let movesArray = document.getElementsByClassName('poke-move');
 
 function getPokemon(name) {
   axios
     .get('https://pokeapi.co/api/v2/pokemon/' + name)
-    .then((res) => console.log(res.data))
+    .then((res) => {
+      console.log(res.data);
+      //removes blank details element
+      blankDetails.style.display = 'none';
+
+      //grabs first five moves of pokemon into an array
+      let pokemonMoves = res.data.moves
+        .slice(0, 5)
+        .map((move) => move.move.name);
+
+      //tests
+      console.log(pokemonMoves);
+
+      movesArray.innertext = '...pokemonMoves';
+      console.log(movesArray);
+      //adds filled details element
+      filledDetails.style.display = 'block';
+    })
     .catch((err) => {
       if (err.response) {
         //server responded with other than 200/success
@@ -27,4 +51,9 @@ function getPokemon(name) {
     });
 }
 
-getPokemon('8709097');
+//Event listener that calls getPokemon
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const inputValue = input.value;
+  getPokemon(inputValue);
+});
