50226a31d086ad22819b8ede7c104a1e52ba0330
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-03-03 10:15:31 -0500

    feat: Pokemon Details
    tried to uncomment MSW, breaks functionality of update here
    
    [optional footer(s)]

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/* if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
-  worker.start();
-} */
+/* ß */
 
 //DOM selectors
 const form = document.getElementById('search-form');
