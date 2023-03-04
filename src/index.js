4813f7eedf2cb57876476f87514130571fd43849
Matthew Kitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-03-04 08:45:56 -0500

    Pok 18 pok details (#25)
    * chore: adds getPokemon function
    
    [optional body]
    
    [optional footer(s)]
    
    * chore: getPokemon add
    
    [optional body]
    
    [optional footer(s)]
    
    * chore: adds getPokemon
    
    [optional body]
    
    [optional footer(s)]
    
    * feat: Pokemon Details
    
    [optional body]
    
    [optional footer(s)]
    
    * feat: Pokemon PokDetails
    
    [optional body]
    
    [optional footer(s)]
    
    * feat: Pokemon Details section
    
    [optional body]
    
    [optional footer(s)]
    
    * feat: poke Details update
    
    [optional body]
    
    [optional footer(s)]
    
    * feat: Pokemon Details
    
    [optional body]
    
    [optional footer(s)]
    
    * feat: Pokemon Details
    
    tried to uncomment MSW, breaks functionality of update here
    
    [optional footer(s)]
    
    * feat: Poke Details Fixes
    
    [optional body]
    
    [optional footer(s)]
    
    * feat: pokemon details
    
    merged with main branch and search func
    
    [optional footer(s)]

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 } */
 
 //DOM selectors
+
+const blankDetails = document.getElementById('empty-pokemon-details');
+const filledDetails = document.getElementById('filled-pokemon-details');
 const selectElement = document.getElementById('search-option');
 const inputElement = document.querySelector('input[id="search-input"]');
 const errorSpan = document.getElementById('error-span');
@@ -23,7 +26,58 @@ selectElement?.addEventListener('change', (e) => {
 function getPokemon(name) {
   axios
     .get('https://pokeapi.co/api/v2/pokemon/' + name)
-    .then((res) => console.log(res.data))
+    .then((res) => {
+      //removes blank details element
+      blankDetails.style.display = 'none';
+
+      //grabs first five moves of pokemon into an array
+      let pokemonMoves = res.data.moves
+        .slice(0, 5)
+        .map((move) => move.move.name);
+
+      //Add height to table
+      let pokeHeightValue = res.data.height;
+      let HeightInTable = document.getElementById('poke-height');
+      HeightInTable.innerHTML = pokeHeightValue + '"';
+
+      //Add weight to table
+      let pokeWeightValue = res.data.weight;
+      let WeightInTable = document.getElementById('poke-weight');
+      WeightInTable.innerHTML = `${pokeWeightValue} kg`;
+
+      //Add pokemon type to table
+      let pokeTypeValue = res.data.types[0].type.name;
+      let TypeInTable = document.getElementById('poke-type');
+      TypeInTable.innerHTML = pokeTypeValue;
+
+      //Add move1 to DOM
+      let pokeMove1 = pokemonMoves[0];
+      let ListMove1 = document.getElementById('poke-move-1');
+      ListMove1.innerHTML = pokeMove1;
+
+      //Add move2 to DOM
+      let pokeMove2 = pokemonMoves[1];
+      let ListMove2 = document.getElementById('poke-move-2');
+      ListMove2.innerHTML = pokeMove2;
+
+      //Add move3 to DOM
+      let pokeMove3 = pokemonMoves[2];
+      let ListMove3 = document.getElementById('poke-move-3');
+      ListMove3.innerHTML = pokeMove3;
+
+      //Add move4 to DOM
+      let pokeMove4 = pokemonMoves[3];
+      let ListMove4 = document.getElementById('poke-move-4');
+      ListMove4.innerHTML = pokeMove4;
+
+      //Add move5 to DOM
+      let pokeMove5 = pokemonMoves[4];
+      let ListMove5 = document.getElementById('poke-move-5');
+      ListMove5.innerHTML = pokeMove5;
+
+      //adds filled details element
+      filledDetails.style.display = 'block';
+    })
     .catch((err) => {
       if (err.response) {
         //server responded with other than 200/success
