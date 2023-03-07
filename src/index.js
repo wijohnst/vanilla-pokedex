b1915262b8ee16aa2ade5d7242f545b2af93fd1e
mathiuskitchens <69939679+mathiuskitchens@users.noreply.github.com>
2023-03-06 21:30:29 -0500

    feat: PokemonViewer
    Adds viewable picture of pokemon and text with some flair
    
    [optional footer(s)]

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const selectElement = document.getElementById('search-option');
 const inputElement = document.querySelector('input[id="search-input"]');
 const errorSpan = document.getElementById('error-span');
 const goButton = document.querySelector('button[class="go-button"]');
+const pokeImage = document.getElementById('poke-image');
+const pokeName = document.getElementById('poke-name');
 
 //Dynamic Placeholder Text
 selectElement?.addEventListener('change', (e) => {
@@ -21,6 +23,21 @@ selectElement?.addEventListener('change', (e) => {
     inputElement.placeholder = 'Find Pokémon by Name';
   }
 });
+
+//Function to capitalize words
+function capitalizeWords(str) {
+  // Split the string into an array of words
+  let words = str.split(/[\s-]+/);
+
+  // Iterate through each word in the array
+  for (let i = 0; i < words.length; i++) {
+    // Capitalize the first letter of the word and concatenate it with the rest of the word
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+
+  // Join the array of words back into a single string, using hyphens to re-join hyphenated words
+  return words.join('-');
+}
 
 //Use axios to get pokemon response object
 function getPokemon(name) {
@@ -48,35 +65,42 @@ function getPokemon(name) {
       //Add pokemon type to table
       let pokeTypeValue = res.data.types[0].type.name;
       let TypeInTable = document.getElementById('poke-type');
-      TypeInTable.innerHTML = pokeTypeValue;
+      TypeInTable.innerHTML = capitalizeWords(pokeTypeValue);
 
       //Add move1 to DOM
       let pokeMove1 = pokemonMoves[0];
       let ListMove1 = document.getElementById('poke-move-1');
-      ListMove1.innerHTML = pokeMove1;
+      ListMove1.innerHTML = capitalizeWords(pokeMove1);
 
       //Add move2 to DOM
       let pokeMove2 = pokemonMoves[1];
       let ListMove2 = document.getElementById('poke-move-2');
-      ListMove2.innerHTML = pokeMove2;
+      ListMove2.innerHTML = capitalizeWords(pokeMove2);
 
       //Add move3 to DOM
       let pokeMove3 = pokemonMoves[2];
       let ListMove3 = document.getElementById('poke-move-3');
-      ListMove3.innerHTML = pokeMove3;
+      ListMove3.innerHTML = capitalizeWords(pokeMove3);
 
       //Add move4 to DOM
       let pokeMove4 = pokemonMoves[3];
       let ListMove4 = document.getElementById('poke-move-4');
-      ListMove4.innerHTML = pokeMove4;
+      ListMove4.innerHTML = capitalizeWords(pokeMove4);
 
       //Add move5 to DOM
       let pokeMove5 = pokemonMoves[4];
       let ListMove5 = document.getElementById('poke-move-5');
-      ListMove5.innerHTML = pokeMove5;
+      ListMove5.innerHTML = capitalizeWords(pokeMove5);
+
+      //Add sprite image and text
+      pokeImage.src = res.data.sprites['front_default'];
+      pokeName.innerHTML = capitalizeWords(res.data.name);
 
       //adds filled details element
       filledDetails.style.display = 'block';
+
+      //adds image element
+      pokeImage.style.display = 'block';
     })
     .catch((err) => {
       if (err.response) {
